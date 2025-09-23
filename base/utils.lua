4fb8b3e2b4019ac684eb02794be4791b0eedf8c6
minufy <seomeenu@proton.me>
2025-09-24 00:58:35 +0900

    rgb 함수 추가

--- base/utils.lua
+++ base/utils.lua
@@ -1,5 +1,17 @@
 Utils = {}
 
+function rgb(r, g, b)
+    return {r/255, g/255, b/255}
+end
+
+function rgba(r, g, b, a)
+    return {r/255, g/255, b/255, a}
+end
+
+function alpha(rgb, a)
+    return {rgb[1], rgb[2], rgb[3], a}
+end
+
 local function dist(a, b)
     local ax = a.x+a.w/2
     local ay = a.y+a.h/2
