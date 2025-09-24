079a9e30294e58f531c3fd9c45ab5310928cf105
minufy <seomeenu@proton.me>
2025-09-24 10:54:39 +0900

    config 수정

--- data/config.lua
+++ data/config.lua
@@ -1,13 +1,16 @@
 local config = {}
 
-config.res = {}
-config.res.w = 1280/2
-config.res.h = 720/2
+config.res = {
+    w = 1280/2,
+    h = 720/2,
+}
 
-config.input = {}
-config.input.right = {"d", "right"}
+config.input = {
+    right = {"d", "right"},
+}
 
-config.camera = {}
-config.camera.damp = 0.1
+config.camera = {
+    damp = 0.1
+}
 
 return config
