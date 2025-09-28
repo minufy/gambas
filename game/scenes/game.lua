2544901cfe2ce21b64c5364851c0501bde3ea520
minufy <seomeenu@proton.me>
2025-09-28 18:58:14 +0900

    오류수저

--- game/scenes/game.lua
+++ game/scenes/game.lua
@@ -4,7 +4,7 @@ local Player = require("game.objects.player")
 
 function Game:init()
     self.objects = {}
-    Utils:add(self.objects, Player, Res.w/2, Res.h/2)
+    Utils:add(Player, Res.w/2, Res.h/2)
 end
 
 function Game:draw()
