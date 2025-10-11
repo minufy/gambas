4533aa7d35e9db4b34a8f3faa005be6c774af1dd
minufy <seomeenu@proton.me>
2025-10-11 20:02:27 +0900

    hex 함수 삭제

--- base/utils.lua
+++ base/utils.lua
@@ -10,11 +10,6 @@ function alpha(rgb, a)
     return {rgb[1], rgb[2], rgb[3], a}
 end
 
-function Hex(hex)
-    hex = hex:gsub("#","")
-    return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
-end
-
 function ResetColor()
     love.graphics.setColor(1, 1, 1, 1)
 end
